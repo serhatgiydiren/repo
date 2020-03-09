d8228132a1d24d1ffc6fbc6f1d21b7e24d53048b
Serhat Giydiren <serhatgiydiren@gmail.com>
2020-03-09 16:50:35 +0300

    Create modular_exponent_inverse.cpp

---
+++ modular_exponent_inverse.cpp
@@ -0,0 +1,16 @@
+long long mod_exp(long long a, long long b, const long long &m=MDL)
+{
+ long long res=1;
+ a%=m;
+ for(;b;b>>=1)
+ {
+  if (b&1) res=(res*a)%m;
+  a=(a*a)%m;
+ }
+ return res;
+}
+
+long long mod_inv(const long long &a, const long long &m=MDL)
+{
+ return mod_exp(a,m-2,m);
+}
