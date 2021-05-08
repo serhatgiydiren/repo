6bb48ee5653751f282257a6db9fbcac59ffe3052
Serhat Giydiren <serhatgiydiren@gmail.com>
2021-05-08 19:48:06 +0300

    patika v2

--- games/patika/a.cpp
+++ games/patika/a.cpp
@@ -16,7 +16,7 @@ class timer:high_resolution_clock
   rep elapsed_time() const { return duration_cast<milliseconds>(now()-start_time).count();} 
 };
 
-int time_limit=60000;
+int time_limit=100000;
 timer t;
 
 #ifdef LOCAL
@@ -32,6 +32,98 @@ vector < string > inp;
 vector < vector < int > > adj;
 vector < int > dx={1,-1,0,0};
 vector < int > dy={0,0,1,-1};
+
+int max_path_length=0;
+vector < int > best_path;
+
+void print(const vector < int > &path)
+{
+ D(path);
+ auto res=inp;
+ auto raw=path;
+ for(int i=1;i<oc-1;i++)
+ {
+  if (raw[i]==raw[i-1]+n && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='|';
+  else if (raw[i]==raw[i-1]-n && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='|';
+
+  else if (raw[i]==raw[i-1]+1 && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='-';
+  else if (raw[i]==raw[i-1]-1 && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='-';
+
+  else if (raw[i]==raw[i-1]+n && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='L';
+  else if (raw[i]==raw[i-1]-1 && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='L';
+
+  else if (raw[i]==raw[i-1]+1 && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='q';
+  else if (raw[i]==raw[i-1]-n && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='q';
+
+  else if (raw[i]==raw[i-1]+n && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='j';
+  else if (raw[i]==raw[i-1]+1 && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='j';
+
+  else if (raw[i]==raw[i-1]-1 && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='p';
+  else if (raw[i]==raw[i-1]-n && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='p';
+ }
+ for(auto e:res) D(e);
+}
+
+bool check(const vector < int > &path, const set < int > &path_unique)
+{
+ for(int i=0;i<n;i++)
+ {
+  for(int j=0;j<n;j++)
+  {
+   if (inp[i][j]=='0') continue;
+   if (path_unique.find(i*n+j)!=path_unique.end()) continue;
+   int fr=0;
+
+   for(int k=0;k<4;k++)
+   {
+    int new_i=i+dx[k];
+    int new_j=j+dy[k];
+    if (new_i>=0 && new_i<n && new_j>=0 && new_j<n && inp[new_i][new_j]=='1')
+    {
+     if (path_unique.find(new_i*n+new_j)==path_unique.end() || path.back()==(new_i*n+new_j)) fr++;
+    }
+   }
+   
+   if (fr==0) return false;
+  }
+ }
+ return true;
+}
+
+void dfs(const int &idx, const vector < int > &path, const set < int > &path_unique)
+{
+ if (int(path.size())>max_path_length)
+ {
+  max_path_length=int(path.size());
+  best_path=path;
+  D(max_path_length,t.elapsed_time());
+ }
+ if ((int)path.size()==oc)
+ {
+  D("found");
+  print(path);
+  exit(0);
+ }
+  
+ if (t.elapsed_time()>time_limit)
+ {
+  D("expires");
+  print(best_path);
+  exit(0);
+ }
+  
+ for(auto e:adj[idx])
+ {
+  if (path_unique.find(e)==path_unique.end())
+  {
+   auto w=path;
+   auto v=path_unique;
+   w.push_back(e);
+   v.insert(e);
+   if (check(w,v)) dfs(e,w,v);
+  }
+ }
+}
 
 void solve(const int &test_id)
 {
@@ -58,61 +150,7 @@ void solve(const int &test_id)
  }
  D(oc);
  D(adj);
- queue < pair < int , pair < vector < int > , set < int > > > > q;
- q.push({0,{{0},{0}}});
- while(!q.empty())
- {
-  auto f=q.front();
-  q.pop();
-  if ((int)f.second.first.size()==oc)
-  {
-   D("found");
-   D(f.second.first);
-   auto res=inp;
-   auto raw=f.second.first;
-   for(int i=1;i<oc-1;i++)
-   {
-    if (raw[i]==raw[i-1]+n && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='|';
-    else if (raw[i]==raw[i-1]-n && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='|';
-
-    else if (raw[i]==raw[i-1]+1 && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='-';
-    else if (raw[i]==raw[i-1]-1 && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='-';
-
-    else if (raw[i]==raw[i-1]+n && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='L';
-    else if (raw[i]==raw[i-1]-1 && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='L';
-
-    else if (raw[i]==raw[i-1]+1 && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='q';
-    else if (raw[i]==raw[i-1]-n && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='q';
-
-    else if (raw[i]==raw[i-1]+n && raw[i]-1==raw[i+1]) res[raw[i]/n][raw[i]%n]='j';
-    else if (raw[i]==raw[i-1]+1 && raw[i]-n==raw[i+1]) res[raw[i]/n][raw[i]%n]='j';
-
-    else if (raw[i]==raw[i-1]-1 && raw[i]+n==raw[i+1]) res[raw[i]/n][raw[i]%n]='p';
-    else if (raw[i]==raw[i-1]-n && raw[i]+1==raw[i+1]) res[raw[i]/n][raw[i]%n]='p';
-   }
-   for(auto e:res) D(e);
-   exit(0);
-  }
-  
-  if (t.elapsed_time()>time_limit)
-  {
-   D(t.elapsed_time());
-   D(f.second.first.size());
-   exit(0);
-  }
-  
-  for(auto e:adj[f.first])
-  {
-   if (f.second.second.find(e)==f.second.second.end())
-   {
-    auto w=f.second.first;
-    auto v=f.second.second;
-    w.push_back(e);
-    v.insert(e);
-    q.push({e,{w,v}});
-   }
-  }
- }
+ dfs(0,{0},{0});
 }
 
 void solve_cases()
